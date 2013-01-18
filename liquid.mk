f98d7d770caa8e14276c618ba4d1f0ab3c14974b
nocoast <voteforpedro8@gmail.com>
2013-01-18 10:40:33 -0600

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -1,22 +1,19 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Specify phone tech (gsm) before including full_phone <take note.
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := t0lte
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
+# device
 $(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
+
+# phone
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0lte
-PRODUCT_NAME := cm_t0lte
+PRODUCT_NAME := liquid_t0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N7105
 PRODUCT_MANUFACTURER := samsung
