41a75490c728b036d628e57347ccfdcf4d485146
Sean Hoyt <seanhoyt963@gmail.com>
2013-03-03 10:34:55 -0600

    track jdk's defconfig
    Thanks DAGr8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := cyanogenmod_t0lte_defconfig
+TARGET_KERNEL_CONFIG := jdkernel_t0lte_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
 
 # assert
